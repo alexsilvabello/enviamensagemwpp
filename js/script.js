b5a0c114a26297ea3b9b54056ca923fd0a8a86f2
Alexandre Silva <>
2022-06-28 00:41:21 -0300

    limpando testes no js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ document
             codigoPais = document.querySelector('#cdpais').value,
             telefone = document.querySelector('#tl').value,
             msgDev = document.querySelector('#ms').value;
-        /*msgDev = window.encodeURIComponent(msgDev);*/
         let url = `https://api.whatsapp.com/send?phone=${codigoPais}${telefone}&text=${msgDev}`;
         btnEnviar.href = `${url}`;
     });
